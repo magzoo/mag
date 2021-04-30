b996deffefcd809008f78a8532535649b56dd7a3
EI-Castanhas <jose.castanhas2001@gmail.com>
2021-04-30 12:56:48 +0100

    gif

--- app/src/main/java/com/example/magzoo/SplashScreen.java
+++ app/src/main/java/com/example/magzoo/SplashScreen.java
@@ -14,7 +14,6 @@ public class SplashScreen extends AppCompatActivity {
 
 
     public LinearLayout back;
-    SplashMove splashMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +23,6 @@ public class SplashScreen extends AppCompatActivity {
 
         back = findViewById(R.id.back);
 //        back.setBackgroundResource(R.drawable.splash1);
-        splashMove =  new SplashMove();
-        splashMove.execute();
 
 
 
@@ -43,35 +40,6 @@ public class SplashScreen extends AppCompatActivity {
 
 
     //fazer giff again
-    private class SplashMove extends AsyncTask
-    {
-        @Override
-        protected Object doInBackground(Object[] objects) {
-            while (true)
-            {
-                try
-                {
-                    while (true)
-                    {
-                        back.setBackgroundResource(R.drawable.splash1);
-                        TimeUnit.MILLISECONDS.sleep(300);
-                        back.setBackgroundResource(R.drawable.splash2);
-                        TimeUnit.MILLISECONDS.sleep(300);
-                        back.setBackgroundResource(R.drawable.splash3);
-                        TimeUnit.MILLISECONDS.sleep(300);
-                        back.setBackgroundResource(R.drawable.splash4);
-                        TimeUnit.MILLISECONDS.sleep(300);
-                        back.setBackgroundResource(R.drawable.splash5);
-                        TimeUnit.MILLISECONDS.sleep(300);
-                    }
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
 
-        }
-    }
 
 }
